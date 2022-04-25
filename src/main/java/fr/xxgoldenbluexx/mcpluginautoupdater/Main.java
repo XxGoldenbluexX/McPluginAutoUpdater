6a30110603e95f047dd5dd79e4524e877cc08ea3
XxGoldenbluexX <tonyg222@outlook.fr>
2022-04-25 21:25:56 +0200

    change project compliance to at leaste java 8

--- src/main/java/fr/xxgoldenbluexx/mcpluginautoupdater/Main.java
+++ src/main/java/fr/xxgoldenbluexx/mcpluginautoupdater/Main.java
@@ -3,6 +3,8 @@ package fr.xxgoldenbluexx.mcpluginautoupdater;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.StringArgument;
 
 public class Main extends JavaPlugin {
 
@@ -10,6 +12,12 @@ public class Main extends JavaPlugin {
 	public void onEnable() {
 		super.onEnable();
 		CommandAPI.onEnable(this);
+		new CommandAPICommand("updater").withAliases("updt").withArguments(new StringArgument("pluginName")).executes((sender,args)->{
+			
+		}).register();
 	}
 	
+	public static void UpdateCommand() {
+		
+	}
 }
